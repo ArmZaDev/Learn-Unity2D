ccfc1c5f6576300a80304a99e297b2796c6d6261
Nobpharat <nopparat.p62@rsu.ac.th>
2022-06-10 21:22:10 +0700

    Adding a script with Debug.Log inside

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("If you press the up arrow you'll JUMP!!");
+        Debug.Log("If you press the right arrow you'll move!");
+
+        Debug.Log("if you press the space, nothig happens");
+        Debug.Log("if you smash the heybord nothing happens, you just cry");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
